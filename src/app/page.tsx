f014070eb674169e5d6dc684a17165ff2f75c829
iftykhar <s.m.ifty49@gmail.com>
2025-08-23 22:04:43 +0600

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,9 @@
-import { FacilitiesCarousel } from "@/components/landing/facilities-carousel";
-import FindCare from "@/components/shared/FindPerfectCare";
-import Hero from "@/components/shared/Hero";
-import Navbar from "@/components/shared/Navbar";
-import TopBanner from "@/components/shared/TopBanner";
+
 
 export default function Home() {
   return (
     <div>
-      <TopBanner />
-      <Navbar />
-      <Hero />
-      <FacilitiesCarousel />
-      <FindCare />
+    
     </div>
   );
 }
