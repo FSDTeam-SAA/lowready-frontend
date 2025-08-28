67c81c3a8b086e112b719c67dd2a20a404171e71
Md.Habibulla <mdhabibulla146425@gmail.com>
2025-08-28 07:20:00 +0600

    resolve

--- src/components/dashboard/dashboardHead/DashboardHead.tsx
+++ src/components/dashboard/dashboardHead/DashboardHead.tsx
@@ -17,7 +17,7 @@ export function DashboardHeader() {
       <h1 className="text-[24px] text-[#28A745] font-bold capitalize">
         {currentPage}
       </h1>
-
+            {/* <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae porro reiciendis dolorum quos dolor quia temporibus, maxime ad cumque, doloremque placeat deleniti! Excepturi quas enim sint velit officia dolorum esse recusandae, odio voluptate blanditiis? Quasi culpa ullam soluta, eligendi accusamus at quia maxime, architecto eveniet, voluptates eum. Sunt culpa mollitia ipsum, laboriosam modi sed impedit. Quasi asperiores voluptatum dignissimos ad labore, facere explicabo veritatis ea placeat delectus laboriosam voluptatem animi nisi doloribus esse rem et nihil minima iusto magnam sunt! Quam facilis ab culpa vitae molestias tenetur hic! Totam eaque omnis quasi consequatur nobis minima perferendis unde assumenda quas eveniet.</p> */}
       <div className="flex items-center gap-6">
         <BellDot className="h-6 w-6 text-gray-600 cursor-pointer" />
 
