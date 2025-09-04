b075001b0a4915215782692f0f5946713eb342ed
RashedulHaqueRasel1 <rashedulhaquerasel1@gmail.com>
2025-09-05 01:23:07 +0600

    Small Hero Section Update & Some desgin change

--- src/app/(website)/contact-us/page.tsx
+++ src/app/(website)/contact-us/page.tsx
@@ -1,20 +1,20 @@
 import ContactInformation from "@/components/ContactUs/ContactInformation";
 import GetInTouch from "@/components/ContactUs/GetInTouch";
-import FindCare from "@/components/shared/FindPerfectCare";
+import SmallHero from "@/components/shared/SmallHero";
 import React from "react";
 
 export default function page() {
   return (
     <div>
-      <FindCare
+      <SmallHero
         imageSrc="/contact/contactImage1.jpg"
-        heading="Reach Out to ALH Hub for Support & Guidance "
-        subHeading="Our team is ready to assist you with any questions, provide guidance, and help you confidently connect with the most suitable assisted living facilities for your loved ones."
-        buttonText="Get Started"
-        showButton={false}
+        heading="Reach Out to ALH Hub for"
+        subHeading="Support & Guidance"
+        description="Our team is ready to assist you with any questions, provide guidance, and help you confidently connect with the most suitable assisted living facilities for your loved ones."
       />
       <GetInTouch />
-      <ContactInformation  />
+      <ContactInformation />
     </div>
   );
 }
+ 
